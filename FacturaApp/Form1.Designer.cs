afae35dd9c7e7a5e96b93e8abaf342c9b3100da1
kiobc <61806659+kiobc@users.noreply.github.com>
2023-03-26 21:59:42 -0500

    Add files via upload

---
+++ FacturaApp/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace Factura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Fact = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboIva = new System.Windows.Forms.ComboBox();
+            this.lblIdFactura = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblDevolucion = new System.Windows.Forms.Label();
+            this.lblTotatlPagar = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IVA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btnVender = new System.Windows.Forms.Button();
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnReporte);
+            this.panel1.Controls.Add(this.Fact);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboIva);
+            this.panel1.Controls.Add(this.lblIdFactura);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.txtPrecio);
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Controls.Add(this.txtCantidad);
+            this.panel1.Controls.Add(this.lblDevolucion);
+            this.panel1.Controls.Add(this.lblTotatlPagar);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lblPrecio);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dgvLista);
+            this.panel1.Location = new System.Drawing.Point(30, 70);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 463);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Fact
+            // 
+            this.Fact.AutoSize = true;
+            this.Fact.Location = new System.Drawing.Point(235, 19);
+            this.Fact.Name = "Fact";
+            this.Fact.Size = new System.Drawing.Size(61, 13);
+            this.Fact.TabIndex = 24;
+            this.Fact.Text = "Factura N°:";
+            this.Fact.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 149);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "IVA:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboIva
+            // 
+            this.comboIva.FormattingEnabled = true;
+            this.comboIva.Items.AddRange(new object[] {
+            "0",
+            "12"});
+            this.comboIva.Location = new System.Drawing.Point(133, 146);
+            this.comboIva.Name = "comboIva";
+            this.comboIva.Size = new System.Drawing.Size(121, 21);
+            this.comboIva.TabIndex = 22;
+            // 
+            // lblIdFactura
+            // 
+            this.lblIdFactura.AutoSize = true;
+            this.lblIdFactura.Location = new System.Drawing.Point(298, 19);
+            this.lblIdFactura.Name = "lblIdFactura";
+            this.lblIdFactura.Size = new System.Drawing.Size(0, 13);
+            this.lblIdFactura.TabIndex = 21;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(145, 28);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(76, 20);
+            this.txtNombre.TabIndex = 20;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(145, 99);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(76, 20);
+            this.txtPrecio.TabIndex = 19;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEliminar.Location = new System.Drawing.Point(286, 119);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(109, 48);
+            this.btnEliminar.TabIndex = 18;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAgregar.Location = new System.Drawing.Point(286, 57);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(109, 48);
+            this.btnAgregar.TabIndex = 17;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(144, 56);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(76, 20);
+            this.txtCantidad.TabIndex = 15;
+            // 
+            // lblDevolucion
+            // 
+            this.lblDevolucion.AutoSize = true;
+            this.lblDevolucion.Location = new System.Drawing.Point(323, 399);
+            this.lblDevolucion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDevolucion.Name = "lblDevolucion";
+            this.lblDevolucion.Size = new System.Drawing.Size(10, 13);
+            this.lblDevolucion.TabIndex = 13;
+            this.lblDevolucion.Text = "-";
+            // 
+            // lblTotatlPagar
+            // 
+            this.lblTotatlPagar.AutoSize = true;
+            this.lblTotatlPagar.Location = new System.Drawing.Point(92, 399);
+            this.lblTotatlPagar.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotatlPagar.Name = "lblTotatlPagar";
+            this.lblTotatlPagar.Size = new System.Drawing.Size(10, 13);
+            this.lblTotatlPagar.TabIndex = 10;
+            this.lblTotatlPagar.Text = "-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(58, 369);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "TOTAL A PAGAR:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(58, 57);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "CANTIDAD:";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(142, 108);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(10, 13);
+            this.lblPrecio.TabIndex = 5;
+            this.lblPrecio.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(58, 106);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "PRECIO:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PRODUCTO:";
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.AllowUserToAddRows = false;
+            this.dgvLista.AllowUserToDeleteRows = false;
+            this.dgvLista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLista.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column4,
+            this.Column3,
+            this.IVA,
+            this.Column5});
+            this.dgvLista.Location = new System.Drawing.Point(20, 190);
+            this.dgvLista.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.ReadOnly = true;
+            this.dgvLista.RowHeadersVisible = false;
+            this.dgvLista.RowHeadersWidth = 51;
+            this.dgvLista.RowTemplate.Height = 24;
+            this.dgvLista.Size = new System.Drawing.Size(386, 151);
+            this.dgvLista.TabIndex = 0;
+            this.dgvLista.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLista_CellContentClick);
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "PRODUCTO";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "CANTIDAD";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "PRECIO";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // IVA
+            // 
+            this.IVA.HeaderText = "IVA";
+            this.IVA.Name = "IVA";
+            this.IVA.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "TOTAL";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(170, 22);
+            this.label14.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(183, 30);
+            this.label14.TabIndex = 2;
+            this.label14.Text = "PCGERENTE";
+            // 
+            // btnVender
+            // 
+            this.btnVender.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnVender.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVender.Location = new System.Drawing.Point(163, 547);
+            this.btnVender.Margin = new System.Windows.Forms.Padding(2);
+            this.btnVender.Name = "btnVender";
+            this.btnVender.Size = new System.Drawing.Size(163, 52);
+            this.btnVender.TabIndex = 1;
+            this.btnVender.Text = "GUARDAR";
+            this.btnVender.UseVisualStyleBackColor = false;
+            this.btnVender.Click += new System.EventHandler(this.btnVender_Click);
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReporte.Location = new System.Drawing.Point(286, 351);
+            this.btnReporte.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(109, 48);
+            this.btnReporte.TabIndex = 25;
+            this.btnReporte.Text = "REPORTE";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            this.btnReporte.Click += new System.EventHandler(this.btnReporte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(481, 609);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.btnVender);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PCGERENTE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblDevolucion;
+        private System.Windows.Forms.Label lblTotatlPagar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button btnVender;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblIdFactura;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboIva;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IVA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label Fact;
+        private System.Windows.Forms.Button btnReporte;
+    }
+}
+
---
+++ FacturaApp/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+using CrystalDecisions.CrystalReports.Engine;
+using FacturaApp;
+
+namespace Factura
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = "Data Source=DESKTOP-9INOCP4\\SQLEXPRESS;Initial Catalog=FacturaPrueba;User Id=bray;Password=Hellblazer-7;";
+
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombre = txtNombre.Text;
+                int cantidad = int.Parse(txtCantidad.Text);
+                double precio = double.Parse(txtPrecio.Text);
+                int iva = int.Parse(comboIva.SelectedItem.ToString());
+                double subtotal = precio * cantidad;
+                double total = 0;
+                double totalf = 0;
+                if (iva == 12)
+                {
+                    total = subtotal * 0.12;
+                    totalf = precio + total;
+                }
+                else
+                {
+                    totalf = subtotal;
+                }
+
+                DataGridViewRow fila = new DataGridViewRow();
+                fila.CreateCells(dgvLista);
+                fila.Cells[0].Value = nombre;
+                fila.Cells[1].Value = cantidad;
+                fila.Cells[2].Value = precio;
+                fila.Cells[3].Value = iva;
+                fila.Cells[4].Value = totalf;
+                dgvLista.Rows.Add(fila);
+
+                txtNombre.Clear();
+                txtCantidad.Clear();
+                txtPrecio.Clear();
+                obtenerTotal();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+
+        private int ObtenerIdFactura()
+        {
+            int idFactura = 0;
+
+            string query = "SELECT TOP 1 IdFactura FROM EncabezadoFactura ORDER BY IdFactura DESC";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    idFactura = (int)reader["IdFactura"];
+                }
+            }
+
+            return idFactura;
+        }
+
+
+        public void obtenerTotal()
+        {
+            float costot = 0;
+            int contador = 0;
+
+            contador = dgvLista.RowCount;
+
+            for (int i = 0; i < contador; i++)
+            {
+                costot += float.Parse(dgvLista.Rows[i].Cells[4].Value.ToString());
+            }
+
+            lblTotatlPagar.Text = costot.ToString();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult rppta = MessageBox.Show("¿Desea eliminar producto?",
+                    "Eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (rppta == DialogResult.Yes)
+                {
+                    dgvLista.Rows.Remove(dgvLista.CurrentRow);
+                }
+            }
+            catch { }
+            obtenerTotal();
+        }
+
+
+        private void btnVender_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idFactura = ObtenerIdFactura(); // obtiene el último IdFactura de la tabla EncabezadoFactura
+
+                // Obtener la cadena de conexión a la base de datos
+                string connectionString = "Data Source=DESKTOP-9INOCP4\\SQLEXPRESS;Initial Catalog=FacturaPrueba;Integrated Security=True";
+
+                // Crear una nueva instancia de SqlConnection utilizando la cadena de conexión
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                // Abrir la conexión
+                connection.Open();
+
+                // Insertar los valores en la tabla ProductosVendidos
+                foreach (DataGridViewRow row in dgvLista.Rows)
+                {
+                    string nombre = row.Cells[0].Value.ToString();
+                    int cantidad = int.Parse(row.Cells[1].Value.ToString());
+                    double precio = double.Parse(row.Cells[2].Value.ToString());
+                    int iva = int.Parse(row.Cells[3].Value.ToString());
+                    double totalf = double.Parse(row.Cells[4].Value.ToString());
+
+                    string query = "INSERT INTO ProductosVendidos (IdFactura, NombreProducto, Cantidad, PrecioUnitario, IVA, TotalF) VALUES (@idFactura, @nombre, @cantidad, @precio, @iva, @totalf)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@idFactura", idFactura);
+                    command.Parameters.AddWithValue("@nombre", nombre);
+                    command.Parameters.AddWithValue("@cantidad", cantidad);
+                    command.Parameters.AddWithValue("@precio", precio);
+                    command.Parameters.AddWithValue("@iva", iva);
+                    command.Parameters.AddWithValue("@totalf", totalf);
+                    command.ExecuteNonQuery();
+                }
+
+                // Cerrar la conexión
+                connection.Close();
+
+                MessageBox.Show("Gracias por preferirnos");
+
+                this.Close();
+            }
+            catch (SqlException ex)
+            {
+                // Manejar el error si la conexión no se puede establecer correctamente
+                MessageBox.Show("Error al establecer la conexión a la base de datos: " + ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int idFactura = ObtenerIdFactura();
+            lblIdFactura.Text = idFactura.ToString();
+            obtenerTotal();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvLista_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+            Reporte reporte = new Reporte();
+            reporte.ShowDialog();
+        }
+
+    }
+}
